dfc06c4b1600080e57ae3e0d5e24f22c48659658
unknown <AP47178@MOBW41L0037.ind.zensar.com>
2016-09-20 09:57:04 +0530

    message

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-fcm": "1.1.4",
+    "cordova-plugin-whitelist": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
